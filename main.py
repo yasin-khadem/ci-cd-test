028821a120885db372f87aeffdc184b810c40831
yasin-khadem <yasin.khadem77@gmail.com>
2026-04-27 22:03:57 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def say_hi():
+    return "Hello yasin"
+
+if __name__ == "__main__":
+    print(say_hi())
---
+++ test.py
@@ -0,0 +1,4 @@
+from main import say_hi
+
+def test_say_hi():
+    assert say_hi() == "Hello yasin"
